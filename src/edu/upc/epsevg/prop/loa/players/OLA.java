5f2ebd016c579775c14ff3003ee87a42a387b837
roger <roger.robles@estudiantat.upc.edu>
2021-11-30 19:47:06 +0100

    minimax hecho

--- src/edu/upc/epsevg/prop/loa/players/OLA.java
+++ src/edu/upc/epsevg/prop/loa/players/OLA.java
@@ -14,6 +14,7 @@ import java.util.logging.Logger;
 
 /**
  * Jugador aleatori
+ *
  * @author bernat
  */
 public class OLA implements IPlayer, IAuto {
@@ -21,10 +22,7 @@ public class OLA implements IPlayer, IAuto {
     private String name;
     private GameStatus s;
     private int prof;
-    //private Point anterior;
-    private Point from;
-    //private Point fromAnt;
-    private Point to;
+
 
     public OLA(String name, int prof) {
         this.name = name;
@@ -43,60 +41,56 @@ public class OLA implements IPlayer, IAuto {
      * @param s Tauler i estat actual de joc.
      * @return el moviment que fa el jugador.
      */
-    
     @Override
     public Move move(GameStatus s) {
-        
+
         int valor = Integer.MIN_VALUE;
         int alpha = Integer.MIN_VALUE;
         int beta = Integer.MAX_VALUE;
         int pprof = prof;
-        
-        Point fromAnt = new Point(0,0);
-        Point pos = new Point(0,0);
-        
+
+        Point fromAnt = new Point(0, 0);
+        Point pos = new Point(0, 0);
+        Point from = new Point(0, 0);
+        Point to = new Point(0, 0);
+
         CellType color = s.getCurrentPlayer();
         this.s = s;
         int qn = s.getNumberOfPiecesPerColor(color);
-        //ArrayList<Point> pendingPieces = new ArrayList<>();
+
         ArrayList<Point> moviments = new ArrayList<>();
-        
+
         // Obtenim les peces i la seva ubicació
         for (int q = 0; q < qn; q++) {
-            from = s.getPiece(color,q);
+            from = s.getPiece(color, q);
             // Per cada peça obtenim els seus moviments possibles
             moviments = (s.getMoves(from));
-            for(int qt = 0; qt < moviments.size();qt++){
-                to = moviments.remove(0);
-                
-                //fusion de minimax y move
-                if(!s.isGameOver() && s.getMoves(from) != null){
-                    System.out.println("No peto");
-                    GameStatus saux = new GameStatus(s);
-                    
-                    //ArrayList<Point> moviments = saux.getMoves(from);
-                    //to = moviments.remove(0);
-                    saux.movePiece(from, to);
-                    System.out.println("No peto2");
-                    
-                    int valorNou = movMin(s, pprof-1, alpha,beta);
-                    
-                    if(valorNou > valor){
-                        valor = valorNou;
-                        fromAnt = from;
-                        pos = to;
-                    } 
+
+            if (moviments != null) {
+                for (int qt = 0; qt < moviments.size(); qt++) {
+                    to = moviments.remove(0);
+
+                    //fusion de minimax y move
+                    if (!s.isGameOver()) {
+
+                        GameStatus saux = new GameStatus(s);
+                        
+                        saux.movePiece(from, to);
+
+                        int valorNou = movMin(saux, pprof - 1, alpha, beta, CellType.opposite(color));
+                        //int valorNou = 4;
+                        if (valorNou > valor) {
+                            System.out.println("valorNou: "+ valorNou);
+                            valor = valorNou;
+                            fromAnt = from;
+                            pos = to;
+                        }
+                    }
                 }
-                        //return pos;
-                
-                
-                
-                //Point tirada = minimax(s, prof,from,to);
-                //qt = 0;
             }
         }
-
-        return new Move(from, pos, 0, 0, SearchType.MINIMAX);
+     
+        return new Move(fromAnt, pos, 0, 0, SearchType.MINIMAX);
     }
 
     /**
@@ -105,164 +99,147 @@ public class OLA implements IPlayer, IAuto {
      */
     @Override
     public String getName() {
-        return "Random(" + name + ")";
+        return "Minimax(" + name + ")";
     }
-    
-    /**
-     * Función que inicia la ejecución del algoritmo minimax y decide la siguiente
-     * columna en la que pondremos nuestra ficha.
-     * @param pt Tablero actual, sería la raíz de nuestro árbol.
-     * @param pprof profundiad máxima a la que llegará nuestro algoritmo creando
-     * el árbol.
-     * @return retornamos la columna siguiente en la que pondremos nuestra ficha.
-     */
-   /* public Point minimax(GameStatus s, int pprof, Point anterior, Point to){
-        int valor = Integer.MIN_VALUE;
-        int alpha = Integer.MIN_VALUE;
-        int beta = Integer.MAX_VALUE;
-        Point pos = new Point(0,0);
-        from = anterior;
 
-        
-    //    for (int i = 1; i < 8; i++){
-    //        for(int j = 0; j < 8; j++){
-                
-                from = new Point(i,j);
-                
-                
-                if(!s.isGameOver() && s.getMoves(from) != null){
-                    System.out.println("No peto");
-                    GameStatus saux = new GameStatus(s);
-                    
-                    ArrayList<Point> moviments = saux.getMoves(from);
-                    to = moviments.remove(0);
-                    saux.movePiece(from, to);
-                    System.out.println("No peto2");
-                    
-                    int valorNou = movMin(s, pprof-1, alpha,beta);
-                    
-                    if(valorNou > valor){
-                        valor = valorNou;
-                        anterior = from;
-                        pos = to;
-                    } 
-                }
-                        return pos;
-            }
-      //  }
-        
-
-    //} */
-    
     /**
-     * Función que nos devuelve el movimiento con menor valor heurístico de todos los
-     * movimientos estudiados.
-     * @param pt tablero resultante de poner una ficha en una determinada columna.
-     * @param lastcol columna en la que hemos puesto ficha para estudiar el tablero.
+     * Función que nos devuelve el movimiento con menor valor heurístico de
+     * todos los movimientos estudiados.
+     *
+     * @param pt tablero resultante de poner una ficha en una determinada
+     * columna.
+     * @param lastcol columna en la que hemos puesto ficha para estudiar el
+     * tablero.
      * @param pprof número de niveles restantes que le queda al algoritmo por
      * analizar.
-     * @param alpha variable que determina el alfa para realizar la poda alfa-beta.
-     * @param beta variable que determina la beta para realizar la poda alfa-beta.
+     * @param alpha variable que determina el alfa para realizar la poda
+     * alfa-beta.
+     * @param beta variable que determina la beta para realizar la poda
+     * alfa-beta.
      * @return retorna el valor heurístico máximo entre todas las posibilidades
      * comprobadas.
      */
+    public int movMin(GameStatus s, int pprof, int alpha, int beta, CellType color) {
 
-    public int movMin(GameStatus s, int pprof,int alpha, int beta){
-        
-        if(s.GetWinner() == CellType.PLAYER1) {
+        if (s.GetWinner() == CellType.PLAYER1) {
             System.out.println("Guanya MovMin: " + CellType.PLAYER1.toString());
             return Integer.MAX_VALUE;
-        }
-        else if(s.GetWinner() == CellType.PLAYER2) {
+        } else if (s.GetWinner() == CellType.PLAYER2) {
             System.out.println("Guanya MovMin: " + CellType.PLAYER2.toString());
             return Integer.MIN_VALUE;
-        }
-        else if(pprof == 0) {
+        } else if (pprof == 0) {
             return 0;
             // Funció heurística
         }
 
         int value = Integer.MAX_VALUE;
-        
-        for (int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                
-                Point from = new Point(i,j), to;
-                
-                if(!s.isGameOver() && s.getMoves(from) != null){
-                    
-                    GameStatus saux = new GameStatus(s);
-                    
-                    ArrayList<Point> moviments = saux.getMoves(from);
+        int qn = s.getNumberOfPiecesPerColor(color);
+        Point from = new Point(0, 0);
+        Point to = new Point(0, 0);
+
+        //ArrayList<Point> pendingPieces = new ArrayList<>();
+        ArrayList<Point> moviments = new ArrayList<>();
+
+        // Obtenim les peces i la seva ubicació
+        for (int q = 0; q < qn; q++) {
+            from = s.getPiece(color, q);
+            // Per cada peça obtenim els seus moviments possibles
+            moviments = (s.getMoves(from));
+
+            if (moviments != null) {
+                for (int qt = 0; qt < moviments.size(); qt++) {
                     to = moviments.remove(0);
-                    saux.movePiece(from, to);
-                    
-                    value = Math.min(value, movMax(saux, pprof - 1, alpha, beta));
-                    beta = Math.min(value, beta);
-                    
-                    if(alpha >= beta) {
-                        return value;
+
+                    if (!s.isGameOver()) {
+
+                        GameStatus saux = new GameStatus(s);
+                        saux.movePiece(from, to);
+
+                        value = Math.min(value, movMax(saux, pprof - 1, alpha, beta, CellType.opposite(color)));
+                        
+                        beta = Math.min(value, beta);
+
+                        if (alpha >= beta) {
+                            //break;
+                            
+                            return value;
+                        }
+
                     }
-                  
                 }
             }
+
         }
         return value;
     }
 
     /**
-     * Función que nos devuelve el movimiento con mayor valor heurístico de todos los
-     * movimientos estudiados.
-     * @param pt tablero resultante de poner una ficha en una determinada columna.
-     * @param lastcol columna en la que hemos puesto ficha para estudiar el tablero.
+     * Función que nos devuelve el movimiento con mayor valor heurístico de
+     * todos los movimientos estudiados.
+     *
+     * @param pt tablero resultante de poner una ficha en una determinada
+     * columna.
+     * @param lastcol columna en la que hemos puesto ficha para estudiar el
+     * tablero.
      * @param pprof número de niveles restantes que le queda al algoritmo por
      * analizar.
-     * @param alpha variable que determina el alfa para realizar la poda alfa-beta.
-     * @param beta variable que determina la beta para realizar la poda alfa-beta.
+     * @param alpha variable que determina el alfa para realizar la poda
+     * alfa-beta.
+     * @param beta variable que determina la beta para realizar la poda
+     * alfa-beta.
      * @return retorna el valor heurístico máximo entre todas las posibilidades
      * comprobadas.
      */
-    public int movMax(GameStatus s, int pprof,int alpha, int beta){
+    public int movMax(GameStatus s, int pprof, int alpha, int beta, CellType color) {
 
-        if(s.GetWinner() == CellType.PLAYER1) {
+        if (s.GetWinner() == CellType.PLAYER1) {
             System.out.println("Guanya MovMax: " + CellType.PLAYER1.toString());
             return Integer.MAX_VALUE;
-        }
-        else if(s.GetWinner() == CellType.PLAYER2) {
+        } else if (s.GetWinner() == CellType.PLAYER2) {
             System.out.println("Guanya MovMax: " + CellType.PLAYER2.toString());
             return Integer.MIN_VALUE;
-        }
-        else if(pprof == 0) {
+        } else if (pprof == 0) {
             return 0;
             // Funció heurística
         }
 
         int value = Integer.MIN_VALUE;
-        
-        for (int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                
-                Point from = new Point(i,j), to;
-                
-                if(!s.isGameOver() && s.getMoves(from) != null){
-                    
-                    GameStatus saux = new GameStatus(s);
-                    
-                    ArrayList<Point> moviments = saux.getMoves(from);
+        int qn = s.getNumberOfPiecesPerColor(color);
+        Point from = new Point(0, 0);
+        Point to = new Point(0, 0);
+
+        //ArrayList<Point> pendingPieces = new ArrayList<>();
+        ArrayList<Point> moviments = new ArrayList<>();
+
+        // Obtenim les peces i la seva ubicació
+        for (int q = 0; q < qn; q++) {
+            from = s.getPiece(color, q);
+            // Per cada peça obtenim els seus moviments possibles
+            moviments = (s.getMoves(from));
+
+            if (moviments != null) {
+                for (int qt = 0; qt < moviments.size(); qt++) {
                     to = moviments.remove(0);
-                    saux.movePiece(from, to);
-                    
-                    value = Math.max(value, movMin(saux, pprof - 1, alpha, beta));
-                    alpha = Math.max(value, alpha);
-                    
-                    if(alpha >= beta) {
-                        return value;
+
+                    if (!s.isGameOver()) {
+
+                        GameStatus saux = new GameStatus(s);
+                        saux.movePiece(from, to);
+
+                        value = Math.max(value, movMin(saux, pprof - 1, alpha, beta, CellType.opposite(color)));
+                        
+                        alpha = Math.max(value, alpha);
+
+                        if (alpha >= beta) {
+                            //break;
+                            return value;
+                        }
+
                     }
-                  
                 }
             }
+
         }
         return value;
     }
-
 }
